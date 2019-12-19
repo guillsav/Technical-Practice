387d404d60b826c810ecdf4356fbe99e556eefa1
guillsav <guillsav@gmail.com>
2019-12-19 13:05:03 -0500

    added more test to ransome note

--- ransome-note/ransomeNote.test.js
+++ ransome-note/ransomeNote.test.js
@@ -2,12 +2,18 @@ const canConstruct = require('./ransomeNote');
 
 describe('CanConstruct()', () => {
     it('TestCase_1 should return false', () => {
-        magazine = 'aa';
-        note = 'aab';
+        magazine = 'a';
+        note = 'b';
         expect(canConstruct(magazine, note)).toEqual(false);
     });
 
     it('TestCase_2 should return false', () => {
+        magazine = 'aa';
+        note = 'ab';
+        expect(canConstruct(magazine, note)).toEqual(false);
+    });
+
+    it('TestCase_3 should return false', () => {
         magazine = [
             'Send',
             'a',
@@ -31,22 +37,6 @@ describe('CanConstruct()', () => {
         expect(canConstruct(magazine, note)).toEqual(false);
     });
 
-    it('TestCase_3 should return false', () => {
-        magazine = [
-            'dollars',
-            'Send',
-            'midnight',
-            'million',
-            'tomorrow',
-            'night',
-            'by',
-            'tonight',
-            'a'
-        ];
-        note = ['Send', 'a', 'million', 'dollars', 'by', 'midnight', 'tonight'];
-        expect(canConstruct(magazine, note)).toEqual(true);
-    });
-
     it('TestCase_4 should return false', () => {
         magazine = [
             'dollars',
@@ -60,5 +50,27 @@ describe('CanConstruct()', () => {
         ];
         note = ['Send', 'a', 'million', 'dollars', 'by', 'midnight', 'tonight'];
         expect(canConstruct(magazine, note)).toEqual(false);
+    });
+
+    it('TestCase_5 should return true', () => {
+        magazine = 'aab';
+        note = 'aa';
+        expect(canConstruct(magazine, note)).toEqual(true);
+    });
+
+    it('TestCase_6 should return true', () => {
+        magazine = [
+            'dollars',
+            'Send',
+            'midnight',
+            'million',
+            'tomorrow',
+            'night',
+            'by',
+            'tonight',
+            'a'
+        ];
+        note = ['Send', 'a', 'million', 'dollars', 'by', 'midnight', 'tonight'];
+        expect(canConstruct(magazine, note)).toEqual(true);
     });
 });
