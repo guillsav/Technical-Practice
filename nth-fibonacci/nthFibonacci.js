d633b50c73288c7ab8eaa401ae3b19b41fe8fac3
guillsav <guillsav@gmail.com>
2020-03-05 12:12:45 -0500

    added problems

--- nth-fibonacci/nthFibonacci.js
+++ nth-fibonacci/nthFibonacci.js
@@ -1,23 +1,23 @@
 function fib(num) {
-    const cache = [];
-    cache[0] = 0;
-    cache[1] = 0;
-    cache[2] = 1;
+  const cache = []
+  cache[0] = 0
+  cache[1] = 0
+  cache[2] = 1
 
-    for (let i = 3; i < num + 1; i++) {
-        cache[i] = cache[i - 1] + cache[i - 2];
-    }
-    return cache[num];
+  for (let i = 3; i < num + 1; i++) {
+    cache[i] = cache[i - 1] + cache[i - 2]
+  }
+  return cache[num]
 }
 
+// const cache = { 0: 0, 1: 0, 2: 1 }
 // function fib(num) {
-//     const cache = {0: 0, 1: 0, 2: 1};
+//   if (num in cache) {
+//     return cache[num]
+//   }
+//   cache[num] = fib(num - 1) + fib(num - 2)
 
-//     if (num in cache) {
-//         return cache[num];
-//     }
-//     cache[num] = fib(num - 1) + fib(num - 2);
-//     return cache[num];
+//   return cache[num]
 // }
 
-module.exports = fib;
+console.log(fib(5))
--- two-sum/twoSum.js
+++ two-sum/twoSum.js
@@ -1,13 +1,13 @@
 function twoSum(nums, target) {
-    const numbers = {};
-    for (let i = 0; i < nums.length; i++) {
-        if (target - nums[i] in numbers) {
-            return [numbers[target - nums[i]], i];
-        } else {
-            numbers[nums[i]] = i;
-        }
+  const numbers = {}
+  for (let i = 0; i < nums.length; i++) {
+    if (target - nums[i] in numbers) {
+      return [numbers[target - nums[i]], i]
+    } else {
+      numbers[nums[i]] = i
     }
-    return [];
+  }
+  return []
 }
 
-module.exports = twoSum;
+module.exports = twoSum
